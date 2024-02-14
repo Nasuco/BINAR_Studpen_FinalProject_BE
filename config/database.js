39220d6e12eb1b82c778930e941b66cd7cc5b562
Annas Abdulloh <annasabdulloh18@gmail.com>
2024-02-14 14:24:18 +0700

    update

--- config/database.js
+++ config/database.js
@@ -1,25 +1,32 @@
 import { Sequelize } from 'sequelize';
-import * as pg from 'pg';
 import dotenv from 'dotenv';
 
 dotenv.config();
 
 /** Destruct environment variable to get database configuration */
 const {
-  DB_USERNAME = process.env.DB_USERNAME,
-  DB_PASSWORD = process.env.DB_PASSWORD,
-  DB_HOST = process.env.DB_HOST,
-  DB_NAME = process.env.DB_NAME,
-  DB_PORT = process.env.DB_PORT
+  DB_USERNAME,
+  DB_PASSWORD,
+  DB_HOST,
+  DB_NAME,
+  DB_PORT
 } = process.env;
 
 /** Initialize Sequelize connection */
-const sequelize = new Sequelize(DB_NAME, DB_USERNAME, DB_PASSWORD, {
+const sequelize = new Sequelize({
+  dialect: 'postgres',
   host: DB_HOST,
   port: DB_PORT,
-  dialect: 'postgres',
-  dialectModule: pg,
-  timezone: '+07:00',
-})
+  database: DB_NAME,
+  username: DB_USERNAME,
+  password: DB_PASSWORD,
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false // Add this line if you're getting SSL error
+    }
+  },
+  timezone: '+07:00'
+});
 
 export default sequelize;
