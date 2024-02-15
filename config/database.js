1f3c8ef8017eea1e7a7c6a9642d06111ecb02fc1
Annas Abdulloh <annasabdulloh18@gmail.com>
2024-02-15 07:13:20 +0700

    update db

--- config/database.js
+++ config/database.js
@@ -3,29 +3,23 @@
 const { Pool } = require('pg');
 require('dotenv').config();
 
-const {
-  POSTGRES_USER,
-  POSTGRES_PASSWORD,
-  POSTGRES_HOST,
-  POSTGRES_DATABASE,
-  POSTGRES_URL
-} = process.env;
+const { POSTGRES_URL } = process.env;
 
 module.exports = {
   development: {
-    connectionString: `postgres://${POSTGRES_USER}:${POSTGRES_PASSWORD}@${POSTGRES_HOST}:5432/${POSTGRES_DATABASE}?sslmode=require`,
+    connectionString: `${POSTGRES_URL}?sslmode=require`,
     dialect: "postgres",
     dialectModule: Pool,
     timezone: '+07:00',
   },
   test: {
-    connectionString: `postgres://${POSTGRES_USER}:${POSTGRES_PASSWORD}@${POSTGRES_HOST}:5432/${POSTGRES_DATABASE}?sslmode=require`,
+    connectionString: `${POSTGRES_URL}?sslmode=require`,
     dialect: "postgres",
     dialectModule: Pool,
     timezone: '+07:00',
   },
   production: {
-    connectionString: `postgres://${POSTGRES_USER}:${POSTGRES_PASSWORD}@${POSTGRES_HOST}:5432/${POSTGRES_DATABASE}?sslmode=require`,
+    connectionString: `${POSTGRES_URL}?sslmode=require`,
     dialect: "postgres",
     dialectModule: Pool,
     timezone: '+07:00',
